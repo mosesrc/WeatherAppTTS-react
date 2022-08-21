eccf2ff152825bbeddcc1cc1731e49036520a660
mosesrc <ryanchristophermoses@gmail.com>
2022-08-21 16:04:13 -0400

    WEAT-7 Completed Create Account Component 	- It is static 	- no routing or logic added

--- src/components/NewAccountForm_Component/NewAccountForm.js
+++ src/components/NewAccountForm_Component/NewAccountForm.js
@@ -14,12 +14,12 @@ function NewAccountForm() {
   return (
     <div className='container rounded-4 w-50 account-form'>
       <form>
-        <div className='row'>
-          <div className='block-one col-6 p-4'>
+        <div className='row d-flex justify-content-center'>
+          <div className='block-one order-0 col-12 col-lg-6 p-4'>
             <h3>Create Account</h3>
             <p>Sign up to get started</p>
           </div>
-          <div className='block-two d-flex flex-column justify-content-end col-6'>
+          <div className='block-two order-5 order-lg-1 d-flex flex-column justify-content-end col-12 col-lg-6 text-center'>
             <p className='rotate'>
               Already have an account? &nbsp;
               <span>
@@ -27,7 +27,7 @@ function NewAccountForm() {
               </span>
             </p>
           </div>
-          <div className='block-three mt-2 col-6'>
+          <div className='block-three order-1 mt-2 col-12 col-lg-6'>
             <div>
               <label htmlFor='nameInput' className='form-label'>
                 Name
@@ -67,7 +67,7 @@ function NewAccountForm() {
               </div>
             </div>
           </div>
-          <div className='block-four mt-2 col-6'>
+          <div className='block-four order-2 mt-2 col-12 col-lg-6'>
             <div>
               <label htmlFor='exampleInputEmail1' className='form-label'>
                 Email address
@@ -106,12 +106,12 @@ function NewAccountForm() {
               </div>
             </div>
           </div>
-          <div className='block-five mt-4 col-6'>
-            <button type='submit' className='btn btn-primary mt-4 w-100 h-50'>
+          <div className='block-five order-3 col-12 col-lg-6'>
+            <button type='submit' className='btn btn-primary w-100 h-50 mt-4'>
               Create Account
             </button>
           </div>
-          <div className='block-six mt-4 text-center col-6'>
+          <div className='block-six order-4 mb-4 text-center col-12 col-md-6'>
             <h4>OR</h4>
             <p>Sign up using</p>
             <div className='row'>
