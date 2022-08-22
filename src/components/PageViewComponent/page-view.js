2ba5ec0e3e59f51220c1c6700eb4b2a9ce80a7d8
mosesrc <ryanchristophermoses@gmail.com>
2022-08-22 00:12:29 -0400

    WEAT-20 Create Empty List Message 	- Elements render in a separate function

--- src/components/PageViewComponent/page-view.js
+++ src/components/PageViewComponent/page-view.js
@@ -5,13 +5,23 @@ import "./page-view.css";
 import ListView from "../ListComponent/list-view";
 
 function PageView() {
+  // 📝: Empty list Message
+  const emptyList = () => {
+    return (
+      <div>
+        <p>There are no cites in User's list</p>
+        <p>User must search and select a city to view weather information</p>
+      </div>
+    );
+  };
+
   return (
     <div className='container-fluid page-view'>
       <div className='row pt-4 d-flex justify-content-center'>
         <div className='col-4 p-4 list-block rounded-4'>
           <ListView />
         </div>
-        <div className='col-8 view-block rounded-4'></div>
+        <div className='col-8 view-block rounded-4'>{emptyList()}</div>
       </div>
     </div>
   );
